870cbf5c65f1097678bdb30e5529bdd6aebb58aa
stephanie jones <sjones.njones@gmail.com>
2023-11-01 20:46:47 -0500

    not working code

--- SSSSudoku/src/main.jsx
+++ SSSSudoku/src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+
+
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Error from './pages/Error';
+import Game from './pages/gameBoard';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/Game',
+        element: <Game />,
+      },
+      // {
+      //   path: '/Login',
+      //   element: <Login />,
+      // },
+      // {
+      //   path: '/Welcome',
+      //   element: <Welcome />,
+      // },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
