a6460eee75b2667ec700cc3ba3f9579fe9a0d454
stephanie jones <sjones.njones@gmail.com>
2023-11-02 17:50:26 -0500

    game board, not fully functional

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+// Bringing in the required import from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import WelcomeNav from './components/WelcomeNav';
+// import Nav from './components/NavTabs';
+
+function App() {
+  // The Outlet component will conditionally swap between the different pages according to the URL
+  return (
+    <>
+      < WelcomeNav />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,44 @@
+
+
+import ReactDOM from 'react-dom/client'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Error from './pages/Error';
+import Game from './pages/GameBoard';
+import Home from './pages/Home';
+import Instructions from './pages/Instructions';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/Game',
+        element: <Game />,
+      },
+      // {
+      //   path: '/Login',
+      //   element: <Login />,
+      // },
+      {
+        path: '/Instructions',
+        element: <Instructions />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
