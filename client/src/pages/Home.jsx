0327c145d5ad302aabb2eb2ceb33b6fc96081e2c
sam <sammsfs@gmail.com>
2023-11-02 21:06:34 -0400

    day 1 homepage styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,7 @@ export default function Game() {
   };
 
   const cardStyle = {
+    marginTop: "10vh",
     maxWidth: "60%",
     padding: "20px",
   };
@@ -20,20 +21,61 @@ export default function Game() {
   const button1Style = {
     margin: "0 10px", // Add margin between buttons
     width: "180px",
+    backgroundColor: '#ddd',
+    border: 'none',
+    color: 'black',
+    padding: '10px 20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    cursor: 'pointer',
+    borderRadius: '16px'
   };
 
   const button2Style = {
     margin: "0 10px", // Add margin between buttons
     width: "180px",
+    backgroundColor: '#ddd',
+    border: 'none',
+    color: 'black',
+    padding: '10px 20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    cursor: 'pointer',
+    borderRadius: '16px'
   };
 
   const button3Style = {
     margin: "0 10px", // Add margin between buttons
     width: "180px",
+    backgroundColor: '#ddd',
+    border: 'none',
+    color: 'black',
+    padding: '10px 20px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    cursor: 'pointer',
+    borderRadius: '16px'
   };
 
   const imageStyle = {
     width: "90px",
+  };
+
+  const getFormattedDate = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    return mm + '/' + dd + '/' + yyyy;
+  };
+
+  const cardBottomStyle = {
+    marginTop: "20px",
+    textAlign: 'center', // Center align the text
+    fontSize: 'smaller', // Make the text smaller
   };
 
   return (
@@ -47,25 +89,28 @@ export default function Game() {
             <div className="picture">
               <img src={logo} alt="logo to our app" style={imageStyle} />
             </div>
-            <h2 className="card-title">S u d o k u</h2>
-            <p>
+            <h2 className="card-title">Sudoku</h2>
+            <p className="cardDescription">
               Fill in each square, don't repeat numbers in the same box or line
             </p>
             <div style={buttonGroupStyle}>
               <a
                 href="/Instructions"
-                className="btn btn-dark"
+                className="btn"
                 style={button1Style}
               >
                 How to Play
               </a>
-              <a href="/Login" className="btn btn-dark" style={button2Style}>
+              <a href="/Login" className="btn" style={button2Style}>
                 Login
               </a>
-              <a href="/Game" className="btn btn-dark" style={button3Style}>
+              <a href="/Game" className="btn" style={button3Style}>
                 Play
               </a>
             </div>
+            <div className="cardBottom" style={cardBottomStyle}>
+            {getFormattedDate()}
+          </div>
           </div>
         </div>
       </div>
